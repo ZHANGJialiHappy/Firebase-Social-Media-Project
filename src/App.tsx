168e7985a87767defd88692ecf5b6c3db9558c85
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-12 17:51:03 +0100

    Router fnish

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Main} from "./pages/main";
+import {Login} from "./pages/login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
