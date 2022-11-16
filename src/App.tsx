8e88a9f9dff849088d30c9f9224307088241207a
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-16 11:15:12 +0100

    config. firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import {Main} from "./pages/main";
-import {Login} from "./pages/login";
-import {Navbar} from "./components/navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Main/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYf95qhPZuYI4rpT_dZMqriqbZlw7LXy4",
+  authDomain: "react-course-91532.firebaseapp.com",
+  projectId: "react-course-91532",
+  storageBucket: "react-course-91532.appspot.com",
+  messagingSenderId: "181988450242",
+  appId: "1:181988450242:web:c66659ec257832f376005b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
