e41cb039dac2807907866c85448c952a9bc752f7
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-16 14:30:49 +0100

    LogOut

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,15 @@
 import {Link} from "react-router-dom";
 import {auth} from "../config/firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
+import {signOut} from "firebase/auth";
 
 export const Navbar = () => {
     const [user] = useAuthState(auth);
+
+    const signUserOut = async() => {
+        await signOut(auth);
+    };
+
     return (
     <div className="navbar">
         <div className="links">
@@ -12,9 +18,13 @@ export const Navbar = () => {
         </div>
 
         <div className="user">
-            <p> {user?.displayName} </p>
-            <img src={user?.photoURL || ""} width="100" height="100"/>
-            <button> Sign Out </button>
+            {user && (
+            <>
+                <p> {user?.displayName} </p>
+                <img src={user?.photoURL || ""} width="20" height="20"/>
+                <button onClick={signUserOut}> Sign Out </button>
+            </>
+            )}
         </div>
     </div>
     );
