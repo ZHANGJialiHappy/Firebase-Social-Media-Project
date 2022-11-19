0a3c9214b410ca37f78198751ec3aba3ab191f58
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-19 12:54:23 +0100

    post data to firebase

--- src/pages/create-post/create-form.tsx
+++ src/pages/create-post/create-form.tsx
@@ -1,15 +1,39 @@
 import {useForm} from "react-hook-form";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {addDoc, collection} from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+interface CreateFormData {
+    title: string;
+    description: string;
+}
 
 export const CreateForm = () => {
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title."),
-        description: yup.string().required("You must add a title."),
+        description: yup.string().required("You must add a description."),
     });
+    
+    const postsRef = collection(db, "posts");
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors }, 
+    } = useForm<CreateFormData>({
+        resolver: yupResolver(schema),
+    });
+
+    const onCreatePost = (data: CreateFormData) => {
+        console.log(data);
+    };
+
     return (
-    <form>
-        <input placeholder="Title..." />
-        <textarea placeholder="Description..." />
+    <form onSubmit={handleSubmit(onCreatePost)}>
+        <input placeholder="Title..." {...register("title")} />
+        <p style={{color: "red"}}> {errors.title?.message} </p>
+        <textarea placeholder="Description..." {...register("description")} />
+        <p style={{color: "red"}}> {errors.description?.message} </p>
         <input type="submit" />
     </form>
     );
